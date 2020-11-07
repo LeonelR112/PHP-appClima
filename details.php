296c0fceee964c0f51e6d65a65744fd5eabf72ad
Leonel <Leonel.Ruiz112@gmail.com>
2020-11-06 23:49:11 -0300

    implementación de iconos dinámicos

--- details.php
+++ details.php
@@ -4,7 +4,7 @@ require_once "config.php";
 $listCountries = json_decode(file_get_contents('countries.test.json'));
 
 
-$urlBase1 = "http://api.openweathermap.org/data/2.5/weather?id=3433955&appid=" . APIKEY . "&units=metric";
+$urlBase1 = "http://api.openweathermap.org/data/2.5/weather?id=3433955&appid=" . APIKEY . "&units=metric&lang=sp";
 
 if(!isset($_SESSION['country'])){
     $ch = curl_init();
@@ -15,10 +15,14 @@ if(!isset($_SESSION['country'])){
     $json = curl_exec($ch);
     $info = curl_getinfo($ch);
     $infoUrlClima = json_decode($json);
+
+    ini_set('date.timezone', 'America/Argentina/Buenos_Aires');
+    $format = $date->format("H:i");
+
 }
 else{
     $ciudadId = $_SESSION['country'];
-    $setURL = "http://api.openweathermap.org/data/2.5/weather?id=" . $ciudadId ."&appid=" . APIKEY . "&units=metric";
+    $setURL = "http://api.openweathermap.org/data/2.5/weather?id=" . $ciudadId ."&appid=" . APIKEY . "&units=metric&lang=sp";
 
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $setURL);
@@ -28,8 +32,12 @@ else{
     $json = curl_exec($ch);
     $info = curl_getinfo($ch);
     $infoUrlClima = json_decode($json);
-}
 
+    $Time = new DateTime();
+    $Time->format('H:i');
+    $Time->setTimestamp($infoUrlClima->dt);
+
+}
 
 //var_dump($info);
 
@@ -47,41 +55,87 @@ include "./templates/header.php"
                 <div class="row">
                 <div class="col-12 align-items-center">
                     <?php
-                            switch ($infoUrlClima->weather[0]->main) {
-                                case 'Clouds':
-                                    echo "<img src='./img/icons/cloudy.png' width='80px' alt=''>";
+                           if(date("H:i",$infoUrlClima->dt) > strtotime("06:00") && date("H:i",$infoUrlClima->dt) < strtotime("19:00")  ){
+                            switch ($infoUrlClima->weather[0]->description) {
+                                case 'nubes dispersas':
+                                    echo "<img src='./img/icons/cloudy.png' width='150px' alt='nubes'>";
                                     break;
 
-                                case 'Clear':
-                                    echo "<img src='./img/icons/sunny.png' width='80px' alt=''>";
+                                case 'nubes':
+                                    echo "<img src='./img/icons/cloud.png' width='150px' alt='nubes'>";
+                                break;
+
+                                case "lluvia moderada":
+                                    echo "<img src='./img/icons/some_rain.png' width='150px' alt='nubes'>";
+                                break;
+
+                                case 'chubasco de ligera intensidad':
+                                    echo "<img src='./img/icons/some_rain.png' width='150px' alt='nubes'>";
+                                break;
+                                
+                                case 'cielo claro':
+                                    echo "<img src='./img/icons/sunny.png' width='150px' alt=''>";
                                     break;
                                 
-                                case 'Rain':
-                                    echo "<img src='./img/icons/rain.png' width='80px' alt=''>";
+                                case 'lluvia ligera':
+                                    echo "<img src='./img/icons/some_rain.png' width='150px' alt=''>";
                                     break;
 
                                 default:
-                                    echo "<img src='./img/icons/sunny.png' width='80px' alt='imagen'>";
+                                    echo "<img src='./img/icons/sunny.png' width='150px' alt='imagen'>";
                                     break;
                             }
-                            ?>
+                        }
+                        else{
+                            switch ($infoUrlClima->weather[0]->description) {
+                                case 'nubes dispersas':
+                                    echo "<img src='./img/icons/semiClear_night.png' width='150px' alt='nubes'>";
+                                break;
+                                
+                                case 'nubes':
+                                    echo "<img src='./img/icons/cloud.png' width='150px' alt='nubes'>";
+                                break;
+
+                                case "lluvia moderada":
+                                    echo "<img src='./img/icons/some_rain.png' width='150px' alt='nubes'>";
+                                break;
+                                
+                                case 'chubasco de ligera intensidad':
+                                    echo "<img src='./img/icons/some-rain-night.png' width='150px' alt='nubes'>";
+                                break;
+                                
+                                case 'cielo claro':
+                                    echo "<img src='./img/icons/clear_night.png' width='150px' alt=''>";
+                                break;
+                                
+                                case 'lluvia ligera':
+                                    echo "<img src='./img/icons/some-rain-night.png' width='150px' alt=''>";
+                                break;
+
+                                default:
+                                    echo "<img src='./img/icons/sunny.png' width='150px' alt='imagen'>";
+                                break;
+                            }
+                        }
+                    ?>
                     <span class="h2"> <?=$infoUrlClima->main->temp ?> °C</span>
                 </div>
                 <div class="col-12 align-items-center"> 
                     <small class="h5"> Sensación térmica: <?=$infoUrlClima->main->feels_like ?> °C</small>
                 </div>
                     <div class="col-4 infoCiudad">
-                        <ul><!--Verificar esto del horario-->
-                            <li>hora actual: <?=date("H:i",$infoUrlClima->dt) ?> hs</li>
+                        <ul>
+                            <li>hora actual:<?=$Time->format("H:s") ?>  hs</li>
                             <li>min: <?=$infoUrlClima->main->temp_min ?> °C</li>
                             <li>max: <?=$infoUrlClima->main->temp_max ?> °C</li>
-                            <li>hora del amanecer:</li>
+                            <li>hora del amanecer:  </li>
                             <li>hora del atardecer:</li>
                             <li>presión: <?=$infoUrlClima->main->pressure ?> hPa</li>
                         </ul>
                     </div>
                     <div class="col-4 infoCiudad">
                         <ul>
+                            <li>detalles: <?=$infoUrlClima->weather[0]->description?></li>
                             <li>humedad: <?=$infoUrlClima->main->humidity ?>%</li>
                             <li>nubosidad: <?=$infoUrlClima->clouds->all ?> %</li>
                             <li>Viento: <img src="./img/wind.png" style="transform: rotate(<?=$infoUrlClima->wind->deg ?>deg);" width="40px" alt=""> <?=$infoUrlClima->wind->speed ?> km/h</li>
--- index.php
+++ index.php
@@ -16,10 +16,13 @@ if(!isset($_SESSION['country'])){
     $json = curl_exec($ch);
     $info = curl_getinfo($ch);
     $infoUrlClima = json_decode($json);
+
+    ini_set('date.timezone', 'America/Argentina/Buenos_Aires');
+    $format = $date->format("H:i");
 }
 else{
     $ciudadId = $_SESSION['country'];
-    $setURL = "http://api.openweathermap.org/data/2.5/weather?id=" . $ciudadId ."&appid=" . APIKEY . "&units=metric";
+    $setURL = "http://api.openweathermap.org/data/2.5/weather?id=" . $ciudadId ."&appid=" . APIKEY . "&units=metric&lang=es";
 
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $setURL);
@@ -30,6 +33,10 @@ else{
     $json = curl_exec($ch);
     $info = curl_getinfo($ch);
     $infoUrlClima = json_decode($json);
+
+    $Time = new DateTime();
+    $Time->format('H:i');
+    $Time->setTimestamp($infoUrlClima->dt);
     //var_dump($info);
 }
 
@@ -46,37 +53,86 @@ include "./templates/header.php";
     <main class="p-2">
         <div class="container-md mt-5">
             <h1>Resumen</h1>
+            <hr style="border: 1px solid grey;">
             <div class="row justify-content-center">
                 <div class="cartaClima col-12 col-lg-8">
-                    <h1 class="title border-bottom"><?= $infoUrlClima->name ?> | <?=$format ?>hs</h1>
+                    <h1 class="title border-bottom"><?= $infoUrlClima->name ?> - <?=$infoUrlClima->sys->country?> </h1>
                     <div class="row">
                         <div class="col-12 col-md-3">
                             Tiempo actual
+                            
                             <?php
-                            switch ($infoUrlClima->weather[0]->main) {
-                                case 'Clouds':
-                                    echo "<img src='./img/icons/cloudy.png' width='150px' alt=''>";
+                           if(date("H:i",$infoUrlClima->dt) > strtotime("06:00") && date("H:i",$infoUrlClima->dt) < strtotime("19:00")  ){
+                                switch ($infoUrlClima->weather[0]->description) {
+                                    case 'nubes dispersas':
+                                        echo "<img src='./img/icons/cloudy.png' width='150px' alt='nubes'>";
+                                        break;
+
+                                    case 'nubes':
+                                        echo "<img src='./img/icons/cloud.png' width='150px' alt='nubes'>";
                                     break;
 
-                                case 'Clear':
-                                    echo "<img src='./img/icons/sunny.png' width='150px' alt=''>";
-                                    break;
-                                
-                                case 'Rain':
-                                    echo "<img src='./img/icons/rain.png' width='150px' alt=''>";
+                                    case "lluvia moderada":
+                                        echo "<img src='./img/icons/some_rain.png' width='150px' alt='nubes'>";
                                     break;
 
-                                default:
-                                    echo "<img src='./img/icons/sunny.png' width='150px' alt='imagen'>";
+                                    case 'chubasco de ligera intensidad':
+                                        echo "<img src='./img/icons/some_rain.png' width='150px' alt='nubes'>";
                                     break;
+                                    
+                                    case 'cielo claro':
+                                        echo "<img src='./img/icons/sunny.png' width='150px' alt=''>";
+                                        break;
+                                    
+                                    case 'lluvia ligera':
+                                        echo "<img src='./img/icons/some_rain.png' width='150px' alt=''>";
+                                        break;
+    
+                                    default:
+                                        echo "<img src='./img/icons/sunny.png' width='150px' alt='imagen'>";
+                                        break;
+                                }
+                            }
+                            else{
+                                switch ($infoUrlClima->weather[0]->description) {
+                                    case 'nubes dispersas':
+                                        echo "<img src='./img/icons/semiClear_night.png' width='150px' alt='nubes'>";
+                                    break;
+                                    
+                                    case 'nubes':
+                                        echo "<img src='./img/icons/cloud.png' width='150px' alt='nubes'>";
+                                    break;
+
+                                    case "lluvia moderada":
+                                        echo "<img src='./img/icons/some_rain.png' width='150px' alt='nubes'>";
+                                    break;
+                                    
+                                    case 'chubasco de ligera intensidad':
+                                        echo "<img src='./img/icons/some-rain-night.png' width='150px' alt='nubes'>";
+                                    break;
+                                    
+                                    case 'cielo claro':
+                                        echo "<img src='./img/icons/clear_night.png' width='150px' alt=''>";
+                                    break;
+                                    
+                                    case 'lluvia ligera':
+                                        echo "<img src='./img/icons/some-rain-night.png' width='150px' alt=''>";
+                                    break;
+    
+                                    default:
+                                        echo "<img src='./img/icons/sunny.png' width='150px' alt='imagen'>";
+                                    break;
+                                }
                             }
                             ?>
-
+                            <p>
+                                <h3 class="text-center"><?=$infoUrlClima->weather[0]->description ?></h3>
+                            </p>
                         </div>
                         <div class="infoCiudad col-12 col-md-9">
                             <ul>
                                 <li>
-                                    <h3><?=$infoUrlClima->main->temp ?> °C</h3>
+                                    <h2><?=$infoUrlClima->main->temp ?> °C</h2>
                                 </li>
                                 <li>Sensación térmica: <?= $infoUrlClima->main->feels_like ?> °C </li>
                                 <li>Humedad: <?= $infoUrlClima->main->humidity ?>% </li>
